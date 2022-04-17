d700860ef86cd3aa62a2771cc5b08d6dc9015817
omkar <omkargupta702@gmail.com>
2022-04-18 01:16:37 +0530

    update

--- main.js
+++ main.js
@@ -1,28 +1,34 @@
 let answer = document.getElementById("answer");
-
-inputs = document.querySelectorAll(".buttons input");
+inputs = document.querySelectorAll(".button input");
 
 let screenvalue = '';
 
-inputs.forEach(inp => {
-    inp.addEventListener("click", (e) => {
-        text = e.target.value;
+inputs.forEach(inp => 
+    {
+    inp.addEventListener("click", (e) =>
+     {
+        text =e.target.value;
 
-        if(text == "AC"){
+    if(text == "AC")
+    {
             screenvalue = '';
             answer.value = screenvalue;
         }
-        else if(text == "="){
+        else if(text == "=")
+        {
             answer.value = eval(screenvalue);
         }
-        else if(text == "DEL"){
+        else if(text == "DEL")
+        {
             answer.value = answer.value.substr(0, answer.value.length - 1);
+            screenvalue = answer.value;
         }
         else
         {
             screenvalue += text;
-
             answer.value = screenvalue;
+            
         }
-    });
+        
+    })
 });
